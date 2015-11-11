a7a20582a161e663b2d891791ae99858a1f59fef
falbuquerque <fborges@gmx.com>
2015-11-11 17:49:11 -0200

    Commit inicial

---
+++ Node.py
@@ -0,0 +1,10 @@
+__author__ = 'falbuquerque'
+
+class Node:
+    def __init__(self):
+        nodeId = -1
+        ip = -1
+        prevId = -1
+        prevIp = -1
+        nextId = -1
+        nextIp = -1
---
+++ NodeID_IP.py
@@ -0,0 +1,13 @@
+__author__ = 'falbuquerque'
+
+
+class NodeID_IP:
+    def __init__(self):
+        nodeId = -1
+        ip = -1
+
+    def initNodeID_0_k(self):
+        pass
+
+    def initNodeID_2_k(self):
+        pass
